b94dc0c515e1da993a4e43072fc08e45e43545ee
Vlad Danciu <vdanciu@gmail.com>
2015-12-15 00:39:40 +0200

    Veriunea cu mersul la vale

--- CodeBlocks.py
+++ CodeBlocks.py
@@ -68,6 +68,89 @@ from DroneSimulator import *
 
 # [2900] - [3000] - rezervata pentru asamblor
 
+class GetCostPointer(MACRO):
+    def __init__(self, ptr, x, y):
+        self.ptr = ptr
+        self.x = x
+        self.y = y
+        
+    def expand(self):
+        return [
+            COMMENT(str(self)),
+            LETADD(self.ptr, "COSTS_ADR", self.y),
+            INC(self.ptr, "1"),
+            GETPTR(self.ptr, self.ptr),
+            INC(self.ptr, self.x),
+            INC(self.ptr, "1")
+        ]
+    
+    def __str__(self):
+        return "GetCostPointer in " + str(self.ptr) + " for x:" + str(self.x) + ", y:" + str(self.y)
+
+class GetCostPointerNeighbor(MACRO):
+    UP = 1
+    RIGHT = 2
+    DOWN = 3
+    LEFT = 4
+
+    def __init__(self, left, right, direction):
+        self.left = left
+        self.right = right
+        self.dir = direction
+
+    def expand(self):
+        offset = {
+            GetCostPointerNeighbor.DOWN : "fence_c",
+            GetCostPointerNeighbor.LEFT : "-1",
+            GetCostPointerNeighbor.RIGHT : "1"
+            }
+        if self.dir == GetCostPointerNeighbor.UP:
+            instr = LETSUB(self.left, self.right, "fence_c")
+        else:
+            instr = LETADD(self.left, self.right, offset[self.dir])
+
+        return [
+            COMMENT(str(self)),
+            instr
+        ]
+
+    def __str__(self):
+        return "GetCostPointerNeighbor: " + str(self.left) + " for " + str(self.right) + " direction=" + str(self.dir)
+
+class GetBestCost(MACRO):
+    code = 0
+
+    def __init__(self, left, cost, newcost):
+        self.left = left
+        self.cost = cost
+        self.newcost = newcost
+
+    def expand(self):
+        GetBestCost.code += 1
+        labelmin = "BEST COST " + str(GetBestCost.code)
+        labelend = "BEST COST END " + str(GetBestCost.code)
+        labelold = "BEST COST OLD " + str(GetBestCost.code)
+        return [
+            LDA(self.newcost),
+            JGE(labelmin),
+                LABEL(labelold),
+                LDA(self.cost),
+                JGE(labelend),
+
+            LABEL(labelmin),
+            LDA(self.newcost),
+            SUBA(self.cost),
+            JGE(labelold),
+                LDA(self.newcost),
+
+            LABEL(labelend),
+            STA(self.left),
+        ]
+
+    def __str__(self):
+        return "MaxPositive " + str(self.left) + " = " + \
+               str(self.cost) + " if " + str(self.newcost) + " < 0, otherwise min of the two"
+
 def main():
     program = [
         SYM("UP",        "1"),
@@ -75,7 +158,7 @@ def main():
         SYM("DOWN",      "3"),
         SYM("LEFT",      "4"),
         SYM("HOLD",      "0"),
-        SYM("costs_adr", "4000"),
+        SYM("COSTS_ADR", "4000"),
 
         SYM("r",         "[1]"),
         SYM("c",         "[2]"),
@@ -87,97 +170,148 @@ def main():
         SYM("ptr",       "[3000]"),
         SYM("offset",    "[3001]"),
         SYM("maxrow",    "[3002]"),
-        SYM("maxobs",    "[3003]"),
-        SYM("i",         "[3004]"),
-        SYM("xi",        "[3005]"),
-        SYM("yi",        "[3006]"),
-        SYM("cost",      "[3007]"),
+        SYM("maxcol",    "[3003]"),
+        SYM("maxobs",    "[3004]"),
+        SYM("i",         "[3005]"),
+        SYM("xi",        "[3006]"),
+        SYM("yi",        "[3007]"),
+        SYM("cost",      "[3008]"),
+        SYM("tmp",       "[3009]"),
+        SYM("fence_r",   "[3010]"),
+        SYM("fence_c",   "[3011]"),
+        SYM("fmaxrow",   "[3012]"),
+        SYM("fmaxcol",   "[3013]"),
+        SYM("move",      "[3014]"),
+        SYM("ncost",     "[3015]"),
 
         START("INIT"),
 
         LABEL("INIT"),
-        LETSUB("maxrow", "r", "1"),
-        LETADD("offset", "costs_adr", "r"),
+        LETSUB("maxrow",  "r", "1"),
+        LETSUB("maxcol",  "r", "1"),
+        LETADD("fence_r", "r", "2"),
+        LETADD("fence_c", "r", "2"),
+        LETADD("fmaxrow", "r", "1"),
+        LETADD("fmaxcol", "r", "1"),
+        LETSUB("maxobs",  "numobs", "1"),
 
-        LABEL("INIT LOOP"),
-            LETADD("ptr", "costs_adr", "i"),
-            LETPTR("ptr","offset"),
-            INC("offset", "c"),
-            INC("i", "1"),
-        IFLESS("maxrow", "i","INIT LOOP"),
+        LETADD("offset", "COSTS_ADR", "fence_r"),
 
         LET("i", "0"),
-        LETSUB("maxobs", "numobs", "1"),
+        LABEL("INIT LOOP COSTS"),
+            LETADD("ptr", "COSTS_ADR", "i"),
+            LETPTR("ptr","offset"),
 
-        LABEL("INIT LOOP2"),
+            COMMENT("mark the first and the last elements as -1 to mark the fence"),
+            LETPTR("offset", "-1"),
+            LETADD("tmp", "offset", "fmaxcol"),
+            LETPTR("tmp", "-1"),
+
+            INC("offset", "fence_c"),
+            INC("i", "1"),
+        IFLESS("fmaxrow", "i", "INIT LOOP COSTS"),
+
+        COMMENT("Mark the first and the last row with -1 for the fence"),
+        LET("i", "0"),
+        LET("ptr","COSTS_ADR"),
+        GETPTR("ptr", "ptr"),
+        LET("tmp", "COSTS_ADR"),
+        INC("tmp", "fmaxrow"),
+        GETPTR("tmp", "tmp"),
+        LABEL("INIT LOOP MARK FENCE"),
+            LETPTR("ptr", "-1"),
+            LETPTR("tmp", "-1"),
+            INC("ptr", "1"),
+            INC("tmp", "1"),
+            INC("i", "1"),
+        IFLESS("fmaxcol", "i", "INIT LOOP MARK FENCE"),
+
+
+        LET("yi", "0"),
+        LABEL("INIT LOOP DIST COSTS Y"),
+            LET("xi", "0"),
+            LABEL("INIT LOOP DIST COSTS X"),
+                # cost = abs(tx - xi) + abs(ty - yi)
+                LETSUB("cost", "tx", "xi"),
+                ABS("cost", "cost"),
+                LETSUB("tmp", "ty", "yi"),
+                ABS("tmp", "tmp"),
+                LETADD("cost", "cost", "tmp"),
+                GetCostPointer("ptr", "xi", "yi"),
+                LETPTR("ptr", "cost"),
+                INC("xi", "1"),
+            IFLESS("maxcol", "xi", "INIT LOOP DIST COSTS X"),
+            INC("yi", "1"),
+        IFLESS("maxrow", "yi", "INIT LOOP DIST COSTS Y"),
+
+        LET("i", "0"),
+        LABEL("INIT LOOP OBST"),
             LETADD("ptr", "8", "i"),
             INC("ptr", "i"),
             GETPTR("xi", "ptr"),
 
             INC("ptr", "1"),
             GETPTR("yi", "ptr"),
-
-            LETADD("ptr", "costs_adr", "yi"),
-            GETPTR("ptr", "ptr"),
-            INC("ptr", "xi"),
-            LETPTR("ptr", "-1"),
+            GetCostPointer("ptr", "xi", "yi"),
+            LETPTR("ptr", "-2"),
 
             INC("i", "1"),
-        IFLESS("maxobs", "i", "INIT LOOP2"),
+        IFLESS("maxobs", "i", "INIT LOOP OBST"),
 
-        LABEL("SWITCH TO X"),
-        CHANGE_START("MOVE X"),
+        LABEL ("MOVE"),
+        CHANGE_START("MOVE"),
+        GetCostPointer("ptr", "x", "y"),
+        GETPTR("cost", "ptr"),
 
-        LABEL("MOVE X"),
-        IFLESS("tx", "x", "TX GE X"),
-            LETSUB("xi", "x", "1"),
-            LETADD("ptr", "costs_adr", "y"),
-            GETPTR("ptr", "ptr"),
-            INC("ptr", "xi"),
+        LET("move", "0"),
+        LET("cost", "999999"),
+        GetCostPointerNeighbor("tmp", "ptr", GetCostPointerNeighbor.RIGHT),
+        GETPTR("ncost", "tmp"),
+        GetBestCost("ncost", "cost", "ncost"),
+        IFLESS("ncost", "cost", "MOVE DOWN"),
+            LET("cost", "ncost"),
+            LET("move", "2"),
+
+        LABEL("MOVE DOWN"),
+        GetCostPointerNeighbor("tmp", "ptr", GetCostPointerNeighbor.DOWN),
+        GETPTR("ncost", "tmp"),
+        GetBestCost("ncost", "cost", "ncost"),
+        IFLESS("ncost", "cost", "MOVE LEFT"),
+            LET("cost", "ncost"),
+            LET("move", "3"),
+
+        LABEL("MOVE LEFT"),
+        GetCostPointerNeighbor("tmp", "ptr", GetCostPointerNeighbor.LEFT),
+        GETPTR("ncost", "tmp"),
+        GetBestCost("ncost", "cost", "ncost"),
+        IFLESS("ncost", "cost", "MOVE UP"),
+            LET("cost", "ncost"),
+            LET("move", "4"),
+
+        LABEL("MOVE UP"),
+        GetCostPointerNeighbor("tmp", "ptr", GetCostPointerNeighbor.UP),
+        GETPTR("ncost", "tmp"),
+        GetBestCost("ncost", "cost", "ncost"),
+        IFLESS("ncost", "cost", "MAKE MOVE"),
+            LET("cost", "ncost"),
+            LET("move", "1"),
+
+        LABEL("MAKE MOVE"),
+        IFLESS("0", "move", "MOVE AND HALT"),
             GETPTR("cost", "ptr"),
-            IFLESS("-1", "cost", "SWITCH TO Y"),
-                MOVE_HLT("LEFT"),
-
-        LABEL("TX GE X"),
-        IFLESS("x", "tx", "SWITCH TO Y"),
-            LETADD("xi", "x", "1"),
-            LETADD("ptr", "costs_adr", "y"),
-            GETPTR("ptr", "ptr"),
-            INC("ptr", "xi"),
-            GETPTR("cost", "ptr"),
-            IFLESS("-1", "cost", "SWITCH TO Y"),
-                MOVE_HLT("RIGHT"),
-
-        LABEL("SWITCH TO Y"),
-        CHANGE_START("MOVE Y"),
-
-        LABEL("MOVE Y"),
-        IFLESS("ty", "y", "TY GE Y"),
-            LETSUB("yi", "y", "1"),
-            LETADD("ptr", "costs_adr", "yi"),
-            GETPTR("ptr", "ptr"),
-            INC("ptr", "x"),
-            GETPTR("cost", "ptr"),
-            IFLESS("-1", "cost", "SWITCH TO X"),
-                MOVE_HLT("UP"),
-
-        LABEL("TY GE Y"),
-        IFLESS("y", "ty", "SWITCH TO X"),
-            LETADD("yi", "y", "1"),
-            LETADD("ptr", "costs_adr", "yi"),
-            GETPTR("ptr", "ptr"),
-            INC("ptr", "x"),
-            GETPTR("cost", "ptr"),
-            IFLESS("-1", "cost", "SWITCH TO X"),
-                MOVE_HLT("DOWN")
+            INC("cost", "5"),
+            LETPTR("ptr", "cost"),
+        LABEL("MOVE AND HALT"),
+        MOVE_HLT("move"),
     ]
 
     asm = DroneAssembler(program).\
         compile().\
-        save('/Users/rodanciv/Documents/07_intoTheDark.txt').\
+        spit().\
+        save('/Users/rodanciv/Documents/04_gottaCircleAround_auto.txt').\
         assembly
 
-    dronemem = DroneMemory('/Users/rodanciv/Documents/01_letsGetToKnowEachOther.txt')
+    dronemem = DroneMemory('/Users/rodanciv/Documents/04_gottaCircleAround.txt')
     sim = DroneSimulator(asm)
     while not sim.done:
         sim.memupdate(dronemem.memory)
@@ -186,6 +320,8 @@ def main():
 #        dronemem.refresh()
 
     print sim.status
+    print "score: " + str(sim.getscore(4))
+
 
 if __name__ == "__main__":
     main()
